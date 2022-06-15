5e00d15be307bfd1536e17af187e0e04ab17c611
viacheslavorlov <viacheslavorlov@gmail.com>
2022-06-16 00:44:25 +0300

    коментарии к коду №1

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,16 @@
-import './App.css';
+// импорт базовых компонентов
 import {Component} from 'react';
-
+// импорт частей приложения
+import './App.css';
+import {db} from "../db/db"; // база данных название "words"
 import Header from "../header/header";
-import {db} from "../db/db";
 import AddWordForm from "../add-word-form/add-word-form";
 
 class App extends Component {
 
 	constructor(props) {
 		super(props);
+		// создание состояния для записи нового слова в словарь
 		this.state = {
 			word: '',
 			wordTranslate: '',
@@ -19,6 +21,7 @@ class App extends Component {
 	changeWord = (e) => {
 		let word = document.querySelector(`#${e.target.id}`);
 		this.setState({
+			// использование id элемента для получения ключа в state
 			[e.target.id]: word.value
 		});
 	}
@@ -26,16 +29,19 @@ class App extends Component {
 	addWord = () => {
 		let word = this.state.word;
 		let wordTranslate = this.state.wordTranslate;
-
+		// проверка на наличие слова в словаре
 		db.words.toArray(arr => {
 			if (arr.map(el => el.word + el.wordTranslate).includes(`${word}${wordTranslate}`)) {
+				// если есть - не записывется и выводит alert
 				alert(`${word} as ${wordTranslate} already exists in base`);
 			} else {
+				// усли нет - записывается
 				if (word !== '' && wordTranslate !== '') {
 					db.words.add({word, wordTranslate});
 				}
 			}
 		});
+		// очистка полей ввода
 		this.setState(state => ({...state, name: '', surname: ''}));
 	}
 
