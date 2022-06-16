b13310c6ef57d8f72a87dbbb87a64a524ed03c76
viacheslavorlov <viacheslavorlov@gmail.com>
2022-06-16 21:59:17 +0300

    автоопределение количества слов в словаре начало работы над фильтрафцией

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,19 +5,23 @@ class Header extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			wordsLength: ''
+			wordsLength: '',
+			language: ''
 		}
 	}
 
 	calculateAmountOfWords = () => {
 		db.words.toArray(item => {
 			this.setState(({...this.state, wordsLength: item.length}));
-
 		})
 		// console.log('arr', newArr, 'longitude', longitude.)
-
 	}
 
+	filterWordsByLanguage = (lang) => {
+		db.words.toArray(item => { ///////////////////////
+			item.filter(el => el.language === lang); ///////////////////////
+		});
+	}
 
 	render() {
 		return (
@@ -28,6 +32,7 @@ class Header extends Component {
 					<option value={'english'}>English</option>
 					<option value={'russian'}>Russian</option>
 				</select>
+				<input type="text" id='language' value={this.state.language}/>
 				<p>Всего слов в словаре: <span>{this.state.wordsLength}</span></p>
 			</header>
 		)
